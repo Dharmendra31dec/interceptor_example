1123fb148004c3f288b5c6edc360431ac17fe8d6
Dharmendra31dec <kumardsingh089@gmail.com>
2025-02-04 01:11:17 +0530

    Revert "added field in User Class"
    This reverts commit 9095a83cd81b2399028e37ac7fb98c2304212ca8.

--- src/main/java/com/example/interceptorexample/bean/User.java
+++ src/main/java/com/example/interceptorexample/bean/User.java
@@ -2,31 +2,4 @@ package com.example.interceptorexample.bean;
 
 public class User {
 
-	private Long userid;
-	private String userName;
-	
-	
-	
-	public User() {
-	}
-	public User(Long userid, String userName) {
-		super();
-		this.userid = userid;
-		this.userName = userName;
-	}
-	public Long getUserid() {
-		return userid;
-	}
-	public void setUserid(Long userid) {
-		this.userid = userid;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
-	
-	
 }
